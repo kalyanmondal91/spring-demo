47e26ac5f11107f16e7b68cb7cc59c40ffa19dee
Kalyan Mondal <kalyan.mondal@tesco.com>
2023-05-09 11:24:22 +0530

    Generate Random

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -2,18 +2,26 @@ package com.example.demo;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 @Slf4j
 @RequestMapping(value = "/demo")
 public class DemoController {
-    @GetMapping(value = "/", produces = "application/json")
+    @GetMapping(value = "/random", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
-    public String demo() {
-        return "DemoController";
+    public ResponseEntity<Integer> generateRandom() {
+        return ResponseEntity.ok(RandomNumberGenerate());
+    }
+
+    private int RandomNumberGenerate() {
+        Random rand = new Random();
+        return rand.nextInt(100);
     }
 }
